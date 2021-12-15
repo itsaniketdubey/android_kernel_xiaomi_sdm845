Backup-Gits/kernel_xiaomi_sdm845-wip

kernel: Init drone ci
Change-Id: I6971558fa0f39eaf43f1fda731354c1b3b7ec6b5
Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

kernel_build.sh
@@ -1,0 +1,126 @@
+#! /bin/bash
+# Copyright (C) 2020 KenHV
+# Copyright (C) 2020 Starlight
+# Copyright (C) 2021 CloudedQuartz
+#
+
+# Config
+DEVICE="beryllium"
+DEFCONFIG="${DEVICE}_defconfig"
+LOG="$HOME/log.txt"
+VERSION="8.6"
+
+# Export arch and subarch
+ARCH="arm64"
+SUBARCH="arm64"
+export ARCH SUBARCH
+
+KERNEL_IMG=$KERNEL_DIR/out/arch/$ARCH/boot/Image.gz-dtb
+
+TG_CHAT_ID="$CHAT_ID"
+TG_BOT_TOKEN="$BOT_API_KEY"
+# End config
+
+# Function definitions
+
+# tg_sendinfo - sends text through telegram
+tg_sendinfo() {
+	curl -s "https://api.telegram.org/bot$TG_BOT_TOKEN/sendMessage" \
+		-F parse_mode=html \
+		-F text="${1}" \
+		-F chat_id="${TG_CHAT_ID}" &> /dev/null
+}
+
+# tg_pushzip - uploads final zip to telegram
+tg_pushzip() {
+    curl -F document=@"$1"  "https://api.telegram.org/bot$TG_BOT_TOKEN/sendDocument" \
+            -F chat_id=$TG_CHAT_ID \
+            -F parse_mode=html &> /dev/null
+}
+
+# tg_failed - uploads build log to telegram
+tg_failed() {
+    curl -F document=@"$LOG"  "https://api.telegram.org/bot$TG_BOT_TOKEN/sendDocument" \
+        -F chat_id=$TG_CHAT_ID \
+        -F parse_mode=html &> /dev/null
+}
+
+# build_setup - enter kernel directory and get info for caption.
+# also removes the previous kernel image, if one exists.
+build_setup() {
+    cd "$KERNEL_DIR" || echo -e "\nKernel directory ($KERNEL_DIR) does not exist" || exit 1
+
+    [[ ! -d out ]] && mkdir out
+    [[ -f "$KERNEL_IMG" ]] && rm "$KERNEL_IMG"
+	find . -name "*.dtb" -type f -delete
+}
+
+# build_config - builds .config file for device.
+build_config() {
+	make O=out $1 -j$(nproc --all)
+}
+# build_kernel - builds defconfig and kernel image using llvm tools, while saving the output to a specified log location
+# only use after runing build_setup()
+build_kernel() {
+
+    BUILD_START=$(date +"%s")
+    make -j$(nproc --all) O=out \
+                PATH="$TC_DIR/arm64/bin:$TC_DIR/arm/bin:$PATH" \
+                CROSS_COMPILE=$TC_DIR/arm64/bin/aarch64-elf- \
+                CROSS_COMPILE_ARM32=$TC_DIR/arm/bin/arm-eabi- |& tee $LOG
+
+    BUILD_END=$(date +"%s")
+    DIFF=$((BUILD_END - BUILD_START))
+}
+
+# build_end - creates and sends zip
+build_end() {
+
+	if ! [ -a "$KERNEL_IMG" ]; then
+        echo -e "\n> Build failed, sending logs to Telegram."
+        tg_failed
+        tg_buildtime
+        exit 1
+    fi
+
+    echo -e "\n> Build successful! generating flashable zip..."
+	cd "$AK_DIR" || echo -e "\nAnykernel directory ($AK_DIR) does not exist" || exit 1
+	git clean -fd
+	mv "$KERNEL_IMG" "$AK_DIR"/zImage
+	ZIP_NAME=${KERNELNAME}_${VERSION}_${1}_$(date +%H%M).zip
+	zip -r9 "$ZIP_NAME" ./* -x .git README.md ./*placeholder
+	tg_pushzip "$ZIP_NAME"
+	echo -e "\n> Sent zip through Telegram.\n> File: $ZIP_NAME"
+	tg_buildtime
+}
+
+# tg_buildtime - send build time through tg
+tg_buildtime() {
+	tg_sendinfo "Time taken: <code>$((DIFF / 60))m $((DIFF % 60))s</code>"
+}
+
+# End function definitions
+
+COMMIT=$(git log --pretty=format:"%s" -1)
+COMMIT_SHA=$(git rev-parse --short HEAD)
+KERNEL_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+CAPTION=$(echo -e \
+"HEAD: <code>$COMMIT_SHA: </code><code>$COMMIT</code>
+Branch: <code>$KERNEL_BRANCH</code>")
+
+tg_sendinfo "-- Build Triggered --
+$CAPTION"
+
+# Build device
+build_setup
+build_config $DEFCONFIG
+build_kernel
+build_end $DEVICE
+
+# Build with old touch firmware
+build_setup
+git apply old_touch_fw.patch
+build_config $DEFCONFIG
+build_kernel
+build_end ${DEVICE}_old_touch_fw

kernel_setup.sh
@@ -1,0 +1,33 @@
+#!/bin/bash
+#
+# Copyright (c) 2021 CloudedQuartz
+#
+
+# Script to set up environment to build an android kernel
+# Assumes required packages are already installed
+
+# Config
+CURRENT_DIR="$(pwd)"
+KERNELNAME="SilverCore"
+KERNEL_DIR="$CURRENT_DIR"
+AK_REPO="https://github.com/painkiller3/AnyKernel3"
+AK_DIR="$HOME/AnyKernel3"
+TC_DIR="$HOME"
+# End Config
+
+# clone_tc - clones proton clang to TC_DIR
+clone_tc() {
+	git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 $TC_DIR/arm64
+	git clone --depth=1 https://github.com/mvaisakh/gcc-arm  $TC_DIR/arm
+}
+
+# Clones anykernel
+clone_ak() {
+	git clone $AK_REPO $AK_DIR
+}
+# Actually do stuff
+clone_tc
+clone_ak
+
+# Run build script
+. ${CURRENT_DIR}/kernel_build.sh

itsaniketdubey/android_kernel_xiaomi_sdm845

kernel_build.sh
@@ -1,0 +1,125 @@
+#! /bin/bash
+# Copyright (C) 2020 KenHV
+# Copyright (C) 2020 Starlight
+# Copyright (C) 2021 CloudedQuartz
+#
+
+# Config
+DEVICE="beryllium"
+DEFCONFIG="${DEVICE}_defconfig"
+LOG="$HOME/log.txt"
+VERSION="1.0"
+
+# Export arch and subarch
+ARCH="arm64"
+SUBARCH="arm64"
+export ARCH SUBARCH
+export KBUILD_BUILD_USER=6e6f6f62
+export KBUILD_BUILD_HOST=DroneCI
+export TZ='IST'
+
+KERNEL_IMG=$KERNEL_DIR/out/arch/$ARCH/boot/Image.gz-dtb
+
+TG_CHAT_ID="$CHAT_ID"
+TG_BOT_TOKEN="$BOT_API_KEY"
+# End config
+
+# Function definitions
+
+# tg_sendinfo - sends text through telegram
+tg_sendinfo() {
+	curl -s "https://api.telegram.org/bot$TG_BOT_TOKEN/sendMessage" \
+		-F parse_mode=html \
+		-F text="${1}" \
+		-F chat_id="${TG_CHAT_ID}" &> /dev/null
+}
+
+# tg_pushzip - uploads final zip to telegram
+tg_pushzip() {
+    curl -F document=@"$1"  "https://api.telegram.org/bot$TG_BOT_TOKEN/sendDocument" \
+            -F chat_id=$TG_CHAT_ID \
+            -F caption="$2" \
+            -F parse_mode=html &> /dev/null
+}
+
+# tg_failed - uploads build log to telegram
+tg_failed() {
+    curl -F document=@"$LOG"  "https://api.telegram.org/bot$TG_BOT_TOKEN/sendDocument" \
+        -F chat_id=$TG_CHAT_ID \
+        -F parse_mode=html &> /dev/null
+}
+
+# build_setup - enter kernel directory and get info for caption.
+# also removes the previous kernel image, if one exists.
+build_setup() {
+    cd "$KERNEL_DIR" || echo -e "\nKernel directory ($KERNEL_DIR) does not exist" || exit 1
+
+    [[ ! -d out ]] && mkdir out
+    [[ -f "$KERNEL_IMG" ]] && rm "$KERNEL_IMG"
+	find . -name "*.dtb" -type f -delete
+}
+
+# build_config - builds .config file for device.
+build_config() {
+	make O=out $1 -j$(nproc --all)
+}
+# build_kernel - builds defconfig and kernel image using llvm tools, while saving the output to a specified log location
+# only use after runing build_setup()
+build_kernel() {
+
+    BUILD_START=$(date +"%s")
+    make -j$(nproc --all) O=out \
+                PATH="$TC_DIR/arm64/bin:$TC_DIR/arm/bin:$PATH" \
+                CROSS_COMPILE=$TC_DIR/arm64/bin/aarch64-elf- \
+                CROSS_COMPILE_ARM32=$TC_DIR/arm/bin/arm-eabi- |& tee $LOG
+
+    BUILD_END=$(date +"%s")
+    DIFF=$((BUILD_END - BUILD_START))
+}
+
+# build_end - creates and sends zip
+build_end() {
+
+	if ! [ -a "$KERNEL_IMG" ]; then
+        echo -e "\n> Build failed, sending logs to Telegram."
+        tg_failed
+        tg_buildtime
+        exit 1
+    fi
+
+    echo -e "\n> Build successful! generating flashable zip..."
+	cd "$AK_DIR" || echo -e "\nAnykernel directory ($AK_DIR) does not exist" || exit 1
+	git clean -fd
+	mv "$KERNEL_IMG" "$AK_DIR"/zImage
+	ZIP_NAME=${KERNELNAME}_${COMMIT_SHA}_${1}_$(date +%H%M).zip
+	zip -r9 "$ZIP_NAME" ./* -x .git README.md ./*placeholder
+
+	tg_pushzip "$ZIP_NAME" "Time taken: <code>$((DIFF / 60))m $((DIFF % 60))s</code>"
+	echo -e "\n> Sent zip through Telegram.\n> File: $ZIP_NAME"
+}
+
+# End function definitions
+
+COMMIT=$(git log --pretty=format:"%s" -1)
+COMMIT_SHA=$(git rev-parse --short HEAD)
+KERNEL_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+CAPTION=$(echo -e \
+"HEAD: <code>$COMMIT_SHA: </code><code>$COMMIT</code>
+Branch: <code>$KERNEL_BRANCH</code>")
+
+tg_sendinfo "-- Build Triggered --
+$CAPTION"
+
+# Build device
+build_setup
+build_config $DEFCONFIG
+build_kernel
+build_end $DEVICE
+
+# Build with old touch firmware
+build_setup
+git apply old_touch_fw.patch
+build_config $DEFCONFIG
+build_kernel
+build_end ${DEVICE}_old_touch_fw

kernel_setup.sh
@@ -1,0 +1,33 @@
+#!/bin/bash
+#
+# Copyright (c) 2021 CloudedQuartz
+#
+
+# Script to set up environment to build an android kernel
+# Assumes required packages are already installed
+
+# Config
+CURRENT_DIR="$(pwd)"
+KERNELNAME="6e6f6f62"
+KERNEL_DIR="$CURRENT_DIR"
+AK_REPO="https://github.com/sajidshahriar72543/AnyKernel3"
+AK_DIR="$HOME/AnyKernel3"
+TC_DIR="$HOME"
+# End Config
+
+# clone_tc - clones proton clang to TC_DIR
+clone_tc() {
+	git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 $TC_DIR/arm64
+	git clone --depth=1 https://github.com/mvaisakh/gcc-arm  $TC_DIR/arm
+}
+
+# Clones anykernel
+clone_ak() {
+	git clone $AK_REPO $AK_DIR
+}
+# Actually do stuff
+clone_tc
+clone_ak
+
+# Run build script
+. ${CURRENT_DIR}/kernel_build.sh